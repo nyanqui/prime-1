64d293d4e49252ac68c974a8fc48b56d3825219f
pauNath <yanqui.nathaly@gmail.com>
2015-11-23 21:33:39 -0500

    subir

---
+++ Problema/javascript/app.js
@@ -0,0 +1,10 @@
+$(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+
+    if (scroll >= 600) {
+        
+    	$("header").css("background-color", "#0dc0c0");
+    } else {
+       $("header").css("background-color", "transparent");
+    }
+});
